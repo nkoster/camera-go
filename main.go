1da5ba787caddbfc84d35696f32a87e5c2229a8b
Niels Koster <n.koster@portavita.eu>
2020-01-14 23:18:59 +0100

    Initial, websocket video stream

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func main() {
+	serverHost, serverPort, serverHTML := args()
+	showHTML := ""
+	if serverHTML != "./" {
+		showHTML = "html:" + serverHTML
+	}
+	log.Println("ws2 ->", serverHost+":"+serverPort, showHTML)
+	if serverHTML != "" {
+		fs := http.FileServer(http.Dir(serverHTML))
+		http.Handle("/", fs)
+	}
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		var err error
+		// not safe, only for dev:
+		upgrader.CheckOrigin = func(r *http.Request) bool {
+			return true
+		}
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println("error", err)
+			return
+		}
+		log.Println("connection", r.RemoteAddr)
+		go func(conn *websocket.Conn) {
+			for {
+				mt, data, connErr := conn.ReadMessage()
+				if connErr != nil {
+					log.Println("error", connErr)
+					return
+				}
+				if mt == 1 {
+					log.Println(data)
+				}
+				if mt == 2 {
+					if err := conn.WriteMessage(2, data); err != nil {
+						log.Println("error sending ready message")
+					}
+				}
+			}
+		}(conn)
+	})
+	log.Fatal(http.ListenAndServe(serverHost+":"+serverPort, nil))
+}
