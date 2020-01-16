b38ba577edc290174feced777be84b02b38e0f0d
Niels Koster <n.koster@portavita.eu>
2020-01-16 19:42:47 +0100

    Cleanup

--- main.go
+++ main.go
@@ -52,9 +52,6 @@ func main() {
 							delete(clients, client)
 						}
 					}
-					// if err := conn.WriteMessage(2, data); err != nil {
-					// 	log.Println(err)
-					// }
 				}
 			}
 		}(conn)
