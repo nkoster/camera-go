4ef4f8f94e22157bffb6d57df45b45d8f7af372b
Niels Koster <n.koster@portavita.eu>
2020-02-11 01:19:46 +0100

    Initial

---
+++ update.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp html.temp html.go
+sed -i '/9e31d7ffe4b03a35f666ae495f72964c/ r www/index.html' html.go 
+sed -i '/9e31d7ffe4b03a35f666ae495f72964c/d' html.go
+go build
+scp camera w3b:camera/
+scp www/index.html w3b:camera/
+scp www/drag.html w3b:camera/
+date
