0d49dc237de8b5113ef50669a3ff0f036780ef5e
Niels Koster <niels.koster@portavita.nl>
2020-02-18 16:13:31 +0100

    Do not copy this file

--- update.bash
+++ update.bash
@@ -7,7 +7,6 @@ do
     date
     echo -----------
     scp www/index.html w3b:camera/
-    scp www/drag.html w3b:camera/
     cp html.temp html.go
     sed -i '/9e31d7ffe4b03a35f666ae495f72964c/ r www/index.html' html.go 
     sed -i '/9e31d7ffe4b03a35f666ae495f72964c/d' html.go
