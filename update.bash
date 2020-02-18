488cefb2656639eb36da45a64a14b33aa3a2253a
Niels Koster <n.koster@portavita.eu>
2020-02-19 00:18:58 +0100

    Add philips test screen

--- update.bash
+++ update.bash
@@ -10,9 +10,10 @@ do
     cp html.temp html.go
     sed -i '/9e31d7ffe4b03a35f666ae495f72964c/ r www/index.html' html.go 
     sed -i '/9e31d7ffe4b03a35f666ae495f72964c/d' html.go
+    sed -i '/\ \ \/\*\ 93bc162ec65c2e0eedd58c2fdd8d1fc8\ \*\// r 260px-Philips_PM5544.b64' html.go 
+    sed -i '/\ \ \/\*\ 93bc162ec65c2e0eedd58c2fdd8d1fc8\ \*\//d' html.go 
     go build
     ssh w3b screen -X -S camera quit
     scp camera w3b:camera/
     ssh w3b screen -d -S camera -m ./camera/camera
 done
-
